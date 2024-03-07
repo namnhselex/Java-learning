021d7d31f44fa2da8bc694b5fc5ec31d34ed21fb
namnhselex <nam.nh240401@gmail.com>
2024-03-07 16:04:43 +0700

    First commit

---
+++ src/TestThread.java
@@ -0,0 +1,18 @@
+public class TestThread {
+
+    public static void main(String[] args) {
+
+        OddThread oddThread = new OddThread();
+        EvenThread evenThread = new EvenThread();
+        Thread thread1 = new Thread(oddThread);
+        Thread thread2 = new Thread(evenThread);
+        thread1.start();
+        try{
+            thread1.join();
+        }
+        catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        thread2.start();
+    }
+}
