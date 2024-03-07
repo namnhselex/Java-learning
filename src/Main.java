e86d38e560b56cc8f3c51033d0252dbde364ec99
namnhselex <nam.nh240401@gmail.com>
2024-03-07 17:54:30 +0700

    ///

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,11 @@ import java.util.*;
 
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static int DISTANCE =100;
-    public static int STEP =2;
 
     public static void main(String[] args) {
-
-        Car carA = new Car("A");
-        Car carB = new Car("B");
-        Car carC = new Car("C");
-
-        Thread thread1 = new Thread(carA);
-        Thread thread2 = new Thread(carB);
-        Thread thread3 = new Thread(carC);
-
-        System.out.println("Distance: 100KM");
-        thread1.start();
-        thread2.start();
-        thread3.start();
-
+        Shape shape = ShapeFactory.getShape("CIRCLE");
+        shape.draw();
+        ShapeFactory factory = new ShapeFactory();
+        factory.getShape("SQUARE").draw();
     }
 }
